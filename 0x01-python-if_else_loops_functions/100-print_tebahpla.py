843da7129cf59bf2e157e8f457966226d5532049
putbullet <soulaimanettabaas@gmail.com>
2024-01-24 08:18:28 -0800

    alphabets

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a') - 1, -1):
-    if i % 2 == 0:
-        diff = 0
-    else:
-        diff = 32
+    diff = 32 if i % 2 == 0 else 0
     print('{}'.format(chr(i - diff)), end='')
