29e462acbe36ee0289cba6d194dfd70d4a15d013
putbullet <soulaimanettabaas@gmail.com>
2024-01-26 08:35:33 -0800

    tota

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for matrice in matrix:
         for i in matrice:
-            print("{:d}".format(i), end=" ")
+            print("{:d}".format(i), end=" " if i != matrice[-1] else "")
         print()
