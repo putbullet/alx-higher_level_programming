46f2a338051a582c202059ae990bb31d27f260c3
putbullet <soulaimanettabaas@gmail.com>
2024-01-27 16:44:33 -0800

    dleted

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    divisible_by_2 = False
-    for i in my_list:
-        if i % 2 == 0:
-            divisible_by_2 = True
+    new_list = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            new_list.append(True)
         else:
-            divisible_by_2 = False
-        i += 1
-    return divisible_by_2
+            new_list.append(False)
+    return new_list
