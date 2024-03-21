51c03a473fbd68c050c7ec28f879d801537c6dc0
putbullet <soulaimanettabaas@gmail.com>
2024-03-21 00:26:50 +0000

    gg

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -7,11 +7,11 @@ def magic_string():
     else:
         magic_string.calls = 1
 
-    return ', '.join(['Holberton'] * magic_string.calls)
+    return ', '.join(['BestSchool'] * magic_string.calls)
 
 # Original solution
 #
 # #!/usr/bin/python3
 # def magic_string():
 #     setattr(magic_string, 'rep', getattr(magic_string, 'rep', -1) + 1)
-#     return 'Holberton' + ', Holberton' * getattr(magic_string, 'rep', 0)
+#     return 'BestSchool' + ', BestSchool' * getattr(magic_string, 'rep', 0)
