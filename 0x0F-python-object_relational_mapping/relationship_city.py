4f332ae25f7fa7605115e688e149569ebc0b6ec8
putbullet <soulaimanettabaas@gmail.com>
2024-04-05 02:54:18 +0000

    Commit Success

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -2,7 +2,7 @@
 """
 Contains the class definition of a City
 """
-from model_state import Base
+from relationship_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -15,3 +15,4 @@ class City(Base):
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+
