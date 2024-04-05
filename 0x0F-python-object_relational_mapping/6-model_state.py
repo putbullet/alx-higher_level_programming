7a1260de9351568073e3287afc24c78a07b9dee9
putbullet <soulaimanettabaas@gmail.com>
2024-04-05 01:22:13 +0000

    Commit Success

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
-"""Defines the State class and links it to the 'states' table in the MySQL database."""
-
+"""Start link class to table in database 
+"""
 import sys
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
+from model_state import Base, State
 
-Base = declarative_base()
-
-class State(Base):
-    """Represents a state."""
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(128), nullable=False)
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    # Connect to MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    
-    # Create tables
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""Lists states"""
-
-from sqlalchemy import Column, Integer, String, ForeignKey
+"""
+Contains State class and Base, an instance of declarative_base()
+"""
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
 
 
-class City(Base):
-    """Class representing the states table"""
-    __tablename__ = 'cities'
-
-    id = Column(Integer, nullable=False, primary_key=True,
-                autoincrement=True, unique=True)
+class State(Base):
+    """
+    Class with id and name attributes of each state
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+
