a2d56d6b58e8944500d5c840708260a357c26881
putbullet <soulaimanettabaas@gmail.com>
2024-04-05 01:03:57 +0000

    Commit Success

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Start link class to table in database
+"""Defines the State class and links it to the 'states' table in the MySQL database.
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,6 +7,6 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class State(Base):
-    __tablename__ = 'some_table'
-    id = Column(Integer, primary_key=True)
-    name =  Column(String(128))
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name =  Column(String(128), nullable=False)
