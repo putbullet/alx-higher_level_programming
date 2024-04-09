10e920d06534a7af380ee38abc0768c9a5c0097f
putbullet <soulaimanettabaas@gmail.com>
2024-04-09 00:26:22 +0000

    commit success

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes in a URL and displays all HTTP methods the server will accept.
-curl -s -v -L "$1" | grep -o -E '> (GET|POST|PUT|DELETE|HEAD|OPTIONS|PATCH) '
+curl -sI ALLOW $1 -L | grep "Allow" | cut -d " " -f2-
