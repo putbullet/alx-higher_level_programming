8194d20b3c116aea276001de5293525eb92913b4
putbullet <soulaimanettabaas@gmail.com>
2024-04-12 16:11:29 +0000

    Commit Success

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""
-script that fetches https://alx-intranet.hbtn.io/status
-"""
-from urllib.request import Request, urlopen
+"""Script that fetches https://alx-intranet.hbtn.io/status."""
+import requests
+
 
 if __name__ == "__main__":
-    req = Request("https://alx-intranet.hbtn.io/status")
+    req = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
     print("\t- type: {}".format(type(req.text)))
-    print("\t- content: {}".format(req.text)) 
+    print("\t- content: {}".format(req.text))
