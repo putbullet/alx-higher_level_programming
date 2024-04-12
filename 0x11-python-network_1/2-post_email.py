1d8bd8a5005e67dd73c541fc7dacdba312a0a1a7
putbullet <soulaimanettabaas@gmail.com>
2024-04-12 15:48:42 +0000

    Commit Success

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-takes in a URL and an email, sends a POST request to the passed URL
-with the email as a parameter, and displays the body of the
-response (decoded in utf-8)
+Script that takes in a URL and an email, sends a POST request to the passed
+URL with the email as a parameter, and displays the body of the response
+(decoded in utf-8).
 """
-from urllib.request import Request, urlopen
-from urllib.pare import urlencode
 from sys import argv
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
 
 
 if __name__ == "__main__":
